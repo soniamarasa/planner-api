4c736c06cc5e725ceeaec8a6ec95175c6e86442d
soniamarasa <soniamarasa@hotmail.com>
2022-08-28 20:03:03 -0300

    cors

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ import routes from './routes/routes.js';
 dotenv.config();
 
 const app = express(); //-> iniciando o express na const app
-app.use(cors()); //-> configurando cors
+
+const corsOptions = {
+    origin: '*'
+};
+app.use(cors(corsOptions));
+
 app.use(express.json()); //-> diz para o express para retornar os dados em formato json
 
 const { DB_CONNECTION } = process.env;
